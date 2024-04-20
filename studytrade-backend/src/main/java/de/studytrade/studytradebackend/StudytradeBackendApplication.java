f44a190f99bcc4cc01e46b53b63be1c2bf9e0d1d
Jan <js485@hdm-stuttgart.de>
2024-04-20 14:25:25 +0200

    add: studytrade-backend

---
+++ studytrade-backend/src/main/java/de/studytrade/studytradebackend/StudytradeBackendApplication.java
@@ -0,0 +1,13 @@
+package de.studytrade.studytradebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudytradeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudytradeBackendApplication.class, args);
+	}
+
+}
---
+++ studytrade-backend/src/test/java/de/studytrade/studytradebackend/StudytradeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.studytrade.studytradebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudytradeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
