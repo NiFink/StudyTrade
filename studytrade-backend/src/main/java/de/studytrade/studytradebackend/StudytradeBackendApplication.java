47d5b210d1ed6a0f6ee150b8790e4b88fd3ffa10
Lisa Kohls <lk210@hdm-stuttgart.de>
2024-04-24 12:55:52 +0200

    create-first-endpoints: - connected spring boot to mongo db - create env file for user and password setup

--- studytrade-backend/src/main/java/de/studytrade/studytradebackend/StudytradeBackendApplication.java
+++ studytrade-backend/src/main/java/de/studytrade/studytradebackend/StudytradeBackendApplication.java
@@ -2,8 +2,10 @@ package de.studytrade.studytradebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudytradeBackendApplication {
 
 	public static void main(String[] args) {
