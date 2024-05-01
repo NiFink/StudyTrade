d903b87d5b93f404b23ef6e6960050decb3ec00d
NiFink <Nils@team-fink.de>
2024-05-01 17:34:56 +0200

    Update: Filterbar buttons and Categories

--- studytrade-frontend/src/pages/Shoppage/Components/Filterbar.tsx
+++ studytrade-frontend/src/pages/Shoppage/Components/Filterbar.tsx
@@ -5,9 +5,30 @@ interface FilterBarProps {
 }
 
 function FilterBar({ toggleCategories }: FilterBarProps) {
-  return <div className="flex">
-    
-  </div>;
+  return (<div>
+    <div className=" w-full ">
+      <div className=" flex p-2 justify-center bg-red-200">
+        <button className="md:hidden " onClick={toggleCategories}>
+          <i className="bi bi-filter"></i>
+          Categories
+        </button>
+        <button className="pl-3" onClick={toggleCategories}>
+          <i className="bi bi-funnel-fill"></i>
+          Filter
+        </button>
+        <div>
+        
+          <input type="text" className="border rounded-lg" placeholder="Search" > <i className="bi bi-search"></i></input>
+        </div>
+
+      </div>
+    </div>
+    <div>
+
+    </div>
+  </div>
+
+  );
 }
 
 export default FilterBar;
