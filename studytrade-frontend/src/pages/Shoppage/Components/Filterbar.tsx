26ed753e8f98da4cf95457699a50795971398084
NiFink <Nils@team-fink.de>
2024-05-02 15:56:42 +0200

    Update: Filterbar buttons and Categories

--- studytrade-frontend/src/pages/Shoppage/Components/Filterbar.tsx
+++ studytrade-frontend/src/pages/Shoppage/Components/Filterbar.tsx
@@ -5,27 +5,42 @@ interface FilterBarProps {
 }
 
 function FilterBar({ toggleCategories }: FilterBarProps) {
+
+  const filter = () =>{
+    document.querySelector("#filter")?.classList.toggle("hidden");
+  }
   return (<div>
     <div className=" w-full ">
-      <div className=" flex p-2 justify-center bg-red-200">
+      <div className="flex p-2 justify-center">
         <button className="md:hidden " onClick={toggleCategories}>
           <i className="bi bi-filter"></i>
           Categories
         </button>
-        <button className="pl-3" onClick={toggleCategories}>
+        <button className="pl-3" onClick={filter}>
           <i className="bi bi-funnel-fill"></i>
-          Filter
         </button>
-        <div>
-        
-          <input type="text" className="border rounded-lg" placeholder="Search" > <i className="bi bi-search"></i></input>
+        <div className=" pl-3 ">
+          <input type="search" className="border rounded-lg" placeholder="Search" ></input>
         </div>
+      </div>
+      <div className="flex p-2 justify-center hidden" id="filter"> 
+        <label htmlFor="new" className="">
+          <input type="radio" className="" name="condition" value="new"/>
+            new
+        </label>
+        <label htmlFor="nearly new" className="pl-2">
+          <input type="radio" className="" name="condition" value="nearly new"/>
+          nearly new
+        </label>
+        <label htmlFor="used" className="pl-2">
+          <input type="radio" className="" name="condition" value="used"/>
+          used
+        </label>
 
       </div>
+      
     </div>
-    <div>
 
-    </div>
   </div>
 
   );
