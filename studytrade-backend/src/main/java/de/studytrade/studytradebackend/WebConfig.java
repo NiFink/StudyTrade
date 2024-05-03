4cbb737268fc4e7e2340e003c14a7dad419420f1
Jan <js485@hdm-stuttgart.de>
2024-05-03 15:42:24 +0200

    fix: webConfig

--- studytrade-backend/src/main/java/de/studytrade/studytradebackend/WebConfig.java
+++ studytrade-backend/src/main/java/de/studytrade/studytradebackend/WebConfig.java
@@ -1,1 +1,17 @@
+package de.studytrade.studytradebackend;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
