7039fa383b1ef546821278aa9b5bfb7aca27b98e
NiFink <Nils@team-fink.de>
2024-05-22 17:55:47 +0200

    Update: style from Details and Filter

--- studytrade-frontend/src/pages/Shoppage/Components/ProductDetails.tsx
+++ studytrade-frontend/src/pages/Shoppage/Components/ProductDetails.tsx
@@ -65,7 +65,7 @@ function ProductDetails({
         <div className="flex relative justify-center items-center z-20">
           <div
             ref={detailsRef}
-            className={`fixed top-1 bottom-1 right-2 p-2 lg:w-[500px] md:w-full text-center bg-white ${isDetailsOpen ? "" : "hidden"} rounded-lg`}
+            className={`fixed  top-0 bottom-0 right-0 md:top-2 md:bottom-2 md:right-2 p-2 lg:w-[500px] md:w-full text-center bg-white ${isDetailsOpen ? "" : "hidden"} rounded-lg`}
           >
             <div className="flex p-2 justify-between items-center">
               <button className="p-1.5" onClick={handleCloseDetails}>
