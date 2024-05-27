67c542dd83c5fcea2f72cf1942df66b961fcecce
Jan <js485@hdm-stuttgart.de>
2024-05-27 18:00:06 +0200

    fix: securityConfig

--- studytrade-backend/src/main/java/de/studytrade/studytradebackend/config/SecurityConfig.java
+++ studytrade-backend/src/main/java/de/studytrade/studytradebackend/config/SecurityConfig.java
@@ -6,35 +6,30 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-    @Configuration
-    @EnableWebSecurity
-    public class SecurityConfig  {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
 
-        private final UserDetailsService userDetailsService;
-
-        public SecurityConfig(UserDetailsService userDetailsService) {
-            this.userDetailsService = userDetailsService;
-        }
-
-        @Bean
-        public SecurityFilterChain defaultFilterChain(HttpSecurity httpSecurity) throws Exception {
-            return httpSecurity
-                    .csrf(AbstractHttpConfigurer::disable)
-                    .authorizeHttpRequests(auth-> auth.requestMatchers("/api/v1/users/register","/api/v1/users/error",  "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                            .anyRequest().authenticated())
-                    .httpBasic(Customizer.withDefaults())
-                    .formLogin(Customizer.withDefaults())
-                    .build();
-        }
-
-        @Bean
-        public PasswordEncoder passwordEncoder(){
-            return new BCryptPasswordEncoder();
-        }
+    @Bean
+    public SecurityFilterChain defaultFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/users/register", "/api/v1/users/error", "/swagger-ui/**",
+                                "/v3/api-docs/**")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
