44962686738e8caa0052f21c559270e959efad28
Jan <js485@hdm-stuttgart.de>
2024-05-29 10:56:07 +0200

    fix: allow unauthorized access for testing

--- studytrade-frontend/src/pages/Shoppage/Shoppage.tsx
+++ studytrade-frontend/src/pages/Shoppage/Shoppage.tsx
@@ -49,8 +49,6 @@ function Shoppage({ homepageClick }: ShoppageProps) {
     setCategoryState(category!);
   };
 
-
-
   useEffect(() => {
     fetchProducts();
   }, []);
@@ -83,10 +81,12 @@ function Shoppage({ homepageClick }: ShoppageProps) {
           />
         </div>
         <div className="w-full ">
-          {!products && <div className="flex text-2xl justify-center animate-bounce">
-            Loading...
-            <i className="bi bi-arrow-repeat text-2xl ml-2 animate-spin"></i>
-          </div>}
+          {!products && (
+            <div className="flex text-2xl justify-center animate-bounce">
+              Loading...
+              <i className="bi bi-arrow-repeat text-2xl ml-2 animate-spin"></i>
+            </div>
+          )}
           {products && (
             <ProductList
               products={products as Product[]}
