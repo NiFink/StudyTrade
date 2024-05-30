5a151214ddd29e31e59ec5e8ecd8a62b300880c8
Jan <js485@hdm-stuttgart.de>
2024-05-30 21:24:08 +0200

    add: documentation for swaggerui

--- studytrade-backend/src/main/java/de/studytrade/studytradebackend/controller/UserController.java
+++ studytrade-backend/src/main/java/de/studytrade/studytradebackend/controller/UserController.java
@@ -3,6 +3,8 @@ package de.studytrade.studytradebackend.controller;
 import de.studytrade.studytradebackend.model.AuthUser;
 import de.studytrade.studytradebackend.service.EmailValidatorInterface;
 import de.studytrade.studytradebackend.service.UserInterface;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
@@ -24,12 +26,15 @@ public class UserController {
     private EmailValidatorInterface emailValidatorService;
 
     @GetMapping
+    @Operation(summary = "Get all Users", description = "Get all Users which are listed in the database")
     public ResponseEntity<List<AuthUser>> getAllUsers() {
         return new ResponseEntity<>(userService.allUsers(), HttpStatus.OK);
     }
 
     @PostMapping("/register")
-    public ResponseEntity registerUser(@RequestBody AuthUser user, HttpServletRequest request) {
+    @Operation(summary = "Register a new User", description = "Register a new User with HdM email")
+    public ResponseEntity registerUser(@RequestBody @Schema(description = "User to be registered") AuthUser user,
+            HttpServletRequest request) {
         try {
             if (userService.userExists(user)) {
                 throw new RuntimeException("User already exists");
@@ -44,7 +49,8 @@ public class UserController {
     }
 
     @GetMapping("/verify")
-    public String verifyUser(@Param("code") String code) {
+    @Operation(summary = "Verify a User", description = "Verify a User with verification code")
+    public String verifyUser(@Param("code") @Schema(description = "Verification code") String code) {
         if (userService.verify(code)) {
             return "verify_success";
         } else {
@@ -53,12 +59,16 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<Optional<AuthUser>> getSingleUser(@PathVariable int userId) {
+    @Operation(summary = "Get a single User by ID", description = "Get a single User by ID")
+    public ResponseEntity<Optional<AuthUser>> getSingleUser(
+            @PathVariable @Schema(description = "ID of the User") int userId) {
         return new ResponseEntity<>(userService.singleUser(userId), HttpStatus.OK);
     }
 
     @GetMapping("/{userId}/favorites")
-    public ResponseEntity<List<Integer>> getFavorites(@PathVariable int userId) {
+    @Operation(summary = "Get favorites of a User by ID", description = "Get favorites of a User by ID")
+    public ResponseEntity<List<Integer>> getFavorites(
+            @PathVariable @Schema(description = "ID of the User") int userId) {
         return new ResponseEntity<>(userService.favorites(userId), HttpStatus.OK);
     }
 
@@ -68,7 +78,9 @@ public class UserController {
     }
 
     @PutMapping
-    public ResponseEntity<String> updateUser(@RequestBody AuthUser userRequest) {
+    @Operation(summary = "Update a User", description = "Update a User by ID")
+    public ResponseEntity<String> updateUser(
+            @RequestBody @Schema(description = "User to be updated") AuthUser userRequest) {
         try {
             userService.updateUser(userRequest);
             return new ResponseEntity<>("User updated successfully", HttpStatus.OK);
@@ -79,8 +91,9 @@ public class UserController {
     }
 
     @PutMapping("/{userId}/favorites/{productId}")
-    public ResponseEntity<String> updateFavorites(@PathVariable int userId,
-            @PathVariable int productId) {
+    @Operation(summary = "Update favorites of a User", description = "Update favorites of a User by ID")
+    public ResponseEntity<String> updateFavorites(@PathVariable @Schema(description = "ID of the User") int userId,
+            @PathVariable @Schema(description = "ID of the Product") int productId) {
         try {
             userService.updateFavorites(userId, productId);
             return new ResponseEntity<>("Favorites updated successfully", HttpStatus.OK);
@@ -91,7 +104,8 @@ public class UserController {
     }
 
     @DeleteMapping("/{userId}")
-    public ResponseEntity<String> deleteUser(@PathVariable int userId) {
+    @Operation(summary = "Delete a User by ID", description = "Delete a User by ID")
+    public ResponseEntity<String> deleteUser(@PathVariable @Schema(description = "ID of the User") int userId) {
         try {
             userService.deleteUser(userId);
             return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
@@ -101,7 +115,9 @@ public class UserController {
     }
 
     @DeleteMapping("/{userId}/favorites/{productId}")
-    public ResponseEntity<String> deleteFavorite(@PathVariable int userId, @PathVariable int productId) {
+    @Operation(summary = "Delete a favorite of a User", description = "Delete a favorite of a User by userID and productID")
+    public ResponseEntity<String> deleteFavorite(@PathVariable @Schema(description = "ID of the User") int userId,
+            @PathVariable @Schema(description = "ID of the Product") int productId) {
         try {
             userService.deleteFavorite(userId, productId);
             return new ResponseEntity<>("Favorite deleted successfully", HttpStatus.OK);
