4ae89db1f2efcea927132adb41899420c227525a
NiFink <Nils@team-fink.de>
2024-06-17 18:13:24 +0200

    add: first mocktest

--- studytrade-frontend/src/pages/Shoppage/tests/shoppage.test.tsx
+++ studytrade-frontend/src/pages/Shoppage/tests/shoppage.test.tsx
@@ -1,12 +1,8 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import ProductList from "../Components/ProductList";
 
 const mockProducts = [
   {
-    _id: {
-      $oid: "6627f0876344c07cdf00300e"
-    },
     name: "Used textbook: \"Introduction to Computer Science\"",
     description: "A used textbook for an introduction to computer science, in good condition.",
     category: [
@@ -16,13 +12,11 @@ const mockProducts = [
     price: 29.99,
     img: "./images/computer_science_textbook.jpg",
     productId: 5,
-    creationDate: {
-      $date: "2024-04-28T12:46:50.286Z"
-    },
+    creationDate: "2024-04-28T12:46:50.286Z",
     userId: {
-      $oid: "6651e5e58ae50b7dd1e74add"
+      username: "6651e5e58ae50b7dd1e74add"
     }
-  },
+  }
 ];
 
 
