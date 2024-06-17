e4cdd1c8b6b4721960422161b704aac552e78cf1
Kieß Niklas <nk150@hdm-stuttgart.de>
2024-06-17 13:48:46 +0200

    Documentation Homepage

--- studytrade-frontend/src/pages/Homepage/Components/Arrow.tsx
+++ studytrade-frontend/src/pages/Homepage/Components/Arrow.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
 
 interface ArrowProps {
     arrowClick?: () => void;
     scrollToShopbar: () => void;
 }
-
-const Arrow = ({ arrowClick}: ArrowProps) => {
+{/* displays a arrow as a button to jump down to the shop bar with a backgroundimage */}
+function Arrow ({ arrowClick}: ArrowProps) {
     return (
         <div className="relative flex flex-col items-center justify-center top-24 h-screen overflow-hidden z-0">
-      <img src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2832&amp;q=80" alt="Background" className="absolute inset-0 w-full h-full" />
-      <div className="text-9xl mb-14 -ml-8 font-semibold relative z-10 text-white">
+      <img src="./images/building10.jpg" alt="Background" className="absolute inset-0 w-full h-full" />
+      <div className="text-9xl mb-14 -ml-8 font-semibold relative z-10 text-white [text-shadow:2px_2px_0_#000,-1px_-1px_0_#000,1px_-1px_0_#000,-1px_1px_0_#000,1px_1px_0_#000]">
         StudyTrade
       </div>
       <button
         type="button"
         onClick={arrowClick}
-        className="text-black bg-transparent focus:ring-1 focus:outline-none focus:ring-gray-500 rounded-full text-xl p-6 text-center  inline-flex items-center border-4 border-white w-32 h-32 relative z-10"
+        className="text-black bg-transparent focus:ring-1 focus:outline-none focus:ring-gray-500 rounded-full text-xl p-6 text-center  inline-flex items-center border-4 border-white w-32 h-32 relative z-10 [box-shadow:2px_2px_0_#000,-1px_-1px_0_#000,1px_-1px_0_#000,-1px_1px_0_#000,1px_1px_0_#000]"
       >
       <svg className="w-24 h-24 bi bi-arrow-down" aria-hidden="true" fill="none" viewBox="-1.6 2 13 10">
         <path stroke="white " strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M5 1v12L1 9m4 4L9 9"/>
--- studytrade-frontend/src/pages/Homepage/Components/Shopbar.tsx
+++ studytrade-frontend/src/pages/Homepage/Components/Shopbar.tsx
@@ -3,32 +3,30 @@ interface ShopbarProps {
     profilepageClick?: () => void;
 }
 
+{/* displays the different sites of the website at the end of the landingpage */}
 function Shopbar({ shoppageClick, profilepageClick }: ShopbarProps) {
     return (
-        <div className="mx-8 lg:mx-6 lg:my-[5vh] mt-[21vh] lg:mt-0">
-            <div className="flex flex-col lg:flex-row space-y-6 lg:space-y-0 lg:space-x-7 text-black text-4xl lg:h-[80vh] h-[70vh] overflow-hidden justify-center items-center">
-                <button onClick={shoppageClick} className="w-full lg:w-1/4 h-1/4 lg:h-full rounded-lg relative overflow-hidden flex justify-center items-center">
-                    <img className="absolute inset-0 w-full h-full object-cover rounded-lg hover:rotate-3 hover:scale-125 transition-transform duration-500" src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg" alt="" />
-                    <div className="my-8 z-10 relative mt-3 font-bold">Recommended</div>
-                </button>
-
-                <button onClick={shoppageClick} className="w-full lg:w-1/4 h-1/4 lg:h-full rounded-lg relative overflow-hidden flex justify-center items-center">
-                    <img className="absolute inset-0 w-full h-full object-cover rounded-lg hover:rotate-3 hover:scale-125 transition-transform duration-500" src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg" alt="" />
+        <div className="mx-8 lg:mx-8 lg:my-[5vh] mt-[6vh] lg:mt-0 md:mt-[20vh]">
+            <div className="flex flex-col lg:flex-row space-y-10 lg:space-y-0 lg:space-x-7 text-white text-4xl lg:h-[80vh] h-[70vh] overflow-hidden justify-center items-center ">
+                
+                <button onClick={shoppageClick} className="w-full lg:w-1/3 h-1/3 lg:h-full rounded-lg relative overflow-hidden flex justify-center items-center">
+                    <img className="absolute inset-0 w-full h-full object-cover rounded-lg hover:rotate-3 hover:scale-125 transition-transform duration-500" src="./images/shopcoversmall.jpg" alt="" />
                     <div className="my-8 z-10 relative mt-3 font-bold">Shop</div>
                 </button>
 
-                <button onClick={shoppageClick} className="w-full lg:w-1/4 h-1/4 lg:h-full rounded-lg relative overflow-hidden flex justify-center items-center">
+                <button onClick={shoppageClick} className="w-full lg:w-1/3 h-1/3 lg:h-full rounded-lg relative overflow-hidden flex justify-center items-center">
                     <img className="absolute inset-0 w-full h-full object-cover rounded-lg hover:rotate-3 hover:scale-125 transition-transform duration-500" src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg" alt="" />
                     <div className="my-8 z-10 relative mt-3 font-bold">Favorites</div>
                 </button>
 
-                <button onClick={profilepageClick} className="w-full lg:w-1/4 h-1/4 lg:h-full rounded-lg relative overflow-hidden flex justify-center items-center">
-                    <img className="absolute inset-0 w-full h-full object-cover rounded-lg hover:rotate-3 hover:scale-125 transition-transform duration-500" src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2832&amp;q=80" alt="" />
-                    <div className="my-8 z-10 relative mt-3 font-bold text-white">Profile</div>
+                <button onClick={profilepageClick} className="w-full lg:w-1/3 h-1/3 lg:h-full rounded-lg relative overflow-hidden flex justify-center items-center">
+                    <img className="absolute inset-0 w-full h-full object-cover rounded-lg hover:rotate-3 hover:scale-125 transition-transform duration-500 my-6 mx-8" src="./images/profilcover.jpg" alt="" />
+                    <div className="my-8 z-10 relative mt-3 font-bold">Profile</div>
                 </button>
             </div>
         </div>
     );
 }
+
 
 export default Shopbar;
