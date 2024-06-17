32a0266a76822de858ceaafeb74e4507afe34fbf
kikzer <niklas.kiess@web.de>
2024-06-17 17:20:41 +0200

    add: new pictures for favorites

--- studytrade-frontend/src/pages/Homepage/Components/Shopbar.tsx
+++ studytrade-frontend/src/pages/Homepage/Components/Shopbar.tsx
@@ -16,7 +16,7 @@ function Shopbar({ shoppageClick, profilepageClick }: ShopbarProps) {
                 </button>
 
                 <button onClick={shoppageClick} className="group w-full lg:w-1/3 h-1/3 lg:h-full rounded-lg relative overflow-hidden flex justify-center items-center">
-                    <img className="absolute inset-0 w-full h-full object-cover rounded-lg group-hover:rotate-3 group-hover:scale-125 transition-transform duration-500" src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg" alt="" />
+                    <img className="absolute inset-0 w-full h-full object-cover rounded-lg group-hover:rotate-3 group-hover:scale-125 transition-transform duration-500" src="./images/favorites.jpg" alt="" />
                     <div className="absolute inset-0 bg-gradient-to-b from-transparent via-black/30"></div>
                     <div className="my-8 z-10 relative mt-3 font-bold">Favorites</div>
                 </button>
