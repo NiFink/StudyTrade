034a12cf4c773d5eb2b1c2cdc9f9c391bfbe83c2
jessi <js496@hdm-stuttgart.de>
2024-06-27 13:07:56 +0200

    add: username button, comments, update: size of container etc. #49

--- studytrade-frontend/src/pages/Registerpage/Registerpage.tsx
+++ studytrade-frontend/src/pages/Registerpage/Registerpage.tsx
@@ -15,6 +15,7 @@ function RegisterPage({
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
 
+  {/* callback functions */}
   const backToLogin = () => {
     if (clickBackToLogin) clickBackToLogin();
   };
@@ -25,6 +26,7 @@ function RegisterPage({
       return;
     }
 
+    {/* check whether the mail is an hdm-mail, then registration succes/error message pops up */}
     const emailRegex = /^[a-zA-Z0-9._%+-]+@hdm-stuttgart.de\.([a-zA-Z]{2,})$/;
 
     if (!emailRegex.test(email)) {
@@ -55,6 +57,7 @@ function RegisterPage({
 
   const passwordMatch = password === confirmPassword;
 
+  {/* background and logo */}
   return (
     <div
       className="h-screen flex items-center justify-center bg-light-red"
@@ -64,7 +67,7 @@ function RegisterPage({
         backgroundPosition: "center",
       }}
     >
-      <div className="flex flex-col items-center p-8 bg-white bg-opacity-60 rounded-lg shadow-lg max-w-3xl w-11/12 h-auto">
+      <div className="flex flex-col items-center p-8 bg-white bg-opacity-60 rounded-lg shadow-lg max-w-xl w-11/12 md:w-1/2 lg:w-2/5 h-auto">
         <div className="flex mb-8 md:mb-12">
           <img
             src="/hdm-logo-cut.png"
@@ -72,39 +75,50 @@ function RegisterPage({
             className="w-64 h-auto mr-4 md:mr-6"
           />
         </div>
+        {/* necessary input fields to register */}
+        <input
+          type="username"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="p-3 mb-4 w-11/12 md:w-5/6 lg:w-4/5 border border-gray-300 rounded-lg focus:border-red-500 focus:border-4 transition duration-500 outline-none"
+        />
         <input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="p-3 mb-6 w-full md:w-3/4 lg:w-1/2 border border-gray-300 rounded-lg focus:border-red-500 focus:border-4 transition duration-500 outline-none"
+          className="p-3 mb-4 w-11/12 md:w-5/6 lg:w-4/5 border border-gray-300 rounded-lg focus:border-red-500 focus:border-4 transition duration-500 outline-none"
         />
         <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="p-3 mb-6 w-full md:w-3/4 lg:w-1/2 border border-gray-300 rounded-lg focus:border-red-500 focus:border-4 transition duration-500 outline-none"
+          className="p-3 mb-4 w-11/12 md:w-5/6 lg:w-4/5 border border-gray-300 rounded-lg focus:border-red-500 focus:border-4 transition duration-500 outline-none"
         />
         <input
           type="password"
           placeholder="Confirm Password"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
-          className={`p-3 mb-6 w-full md:w-3/4 lg:w-1/2 border rounded-lg outline-none ${
+          className={`p-3 mb-6 w-11/12 md:w-5/6 lg:w-4/5 border rounded-lg outline-none ${
             passwordMatch ? 'border-gray-300' : 'border-red-500'
           } focus:border-red-500 focus:border-4 transition duration-500`}
         />
+        {/* error massage if passwords do not match */}
         {!passwordMatch && (
           <p className="text-red-500 mb-3 md:mb-4">Passwords do not match</p>
         )}
         {error && <p className="text-red-500 mb-3 md:mb-4">{error}</p>}
+        {/* register button */}
         <button
           onClick={handleRegister}
           className="px-6 py-3 mb-6 cursor-pointer bg-red-600 text-white rounded-lg"
         >
           Register
         </button>
+        {/* redirect to login */}
         <p className="text-base md:text-lg">
           Already have an account?{" "}
           <span
